329a8e381d6bd85c9507dfdd49e7d72f333349a9
Joseph DePompeis <josephdepompeis@gmail.com>
2016-07-21 14:35:55 -0400

    use directives and repeats

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95, description: 'some shitty gem!', canPurchase: true};
+  var gems = [
+    {
+    name: 'Azurite',
+    price: 2.95,
+    description: 'some shitty gem!',
+    canPurchase: true,
+    soldOut: false,
+    },
+    {
+    name: 'Spazuriggle',
+    price: 3.45,
+    description: 'some smelly gem!',
+    canPurchase: true,
+    soldOut: false,
+    }
+    ];
+
   var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
 
 
   });
