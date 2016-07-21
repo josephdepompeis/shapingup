5d6ecb968f60916ef8efc2422b1e1ce6da6e423b
Joseph DePompeis <josephdepompeis@gmail.com>
2016-07-21 14:11:43 -0400

    add button using show directive

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+  var gem = { name: 'Azurite', price: 2.95, description: 'some shitty gem!', canPurchase: true};
   var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function() {
     this.product = gem;
+
 
   });
 
